11628fdccdba156d62c9d3e8d73db0fd6a39bba2
kai-wang <simaow@outlook.com>
2017-06-02 21:23:27 +1000

    first commit

---
+++ src/Demo2.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Concurrent (threadDelay)
+import Foreign.C.Types
+import SDL.Vect
+import qualified SDL
+
+import Paths_sdl2 (getDataFileName)
+
+screenWidth, screenHeight :: CInt
+(screenWidth, screenHeight) = (640, 480)
+
+main :: IO ()
+main = do
+  SDL.initialize [SDL.InitVideo]
+  window <- SDL.createWindow "SDL Tutorial" SDL.defaultWindow { SDL.windowInitialSize = V2 screenWidth screenHeight }
+  SDL.showWindow window
+  screenSurface <- SDL.getWindowSurface window
+
+  helloWorld <- getDataFileName "hello_world.bmp" >>= SDL.loadBMP
+
+  SDL.surfaceBlit helloWorld Nothing screenSurface Nothing
+  SDL.updateWindowSurface window
+
+  threadDelay 5000000
+
+  SDL.destroyWindow window
+  SDL.freeSurface helloWorld
+  SDL.quit
---
+++ src/Paths_sdl2.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -fno-warn-implicit-prelude #-}
+module Paths_sdl2 (
+    version,
+    getDataFileName
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  return ("./data/" ++ name)
